825a79f7cc0a1235446ac6bfbc0e14f431a6251f
Will <willkutler@gmail.com>
2020-05-14 13:02:16 -0400

    further increase rollout timeout

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -33,7 +33,7 @@ make kind-deploy-controller
 
 echo "patch image"
 kubectl patch deployment config-policy-ctrl -n governance -p "{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"config-policy-ctrl\",\"image\":\"${DOCKER_IMAGE_AND_TAG}\"}]}}}}"
-kubectl rollout status -n governance deployment config-policy-ctrl --timeout=120s
+kubectl rollout status -n governance deployment config-policy-ctrl --timeout=180s
 sleep 10
 
 make install-resources
