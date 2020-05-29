1f9af579680384d66ff88669c1c20631ef583379
Yu Cao <ycao@redhat.com>
2020-05-29 17:37:16 -0400

    try

--- build/run-unit-tests.sh
+++ build/run-unit-tests.sh
@@ -41,6 +41,6 @@ go get github.com/onsi/gomega/...
 # run e2e test
 make build-instrumented
 make kind-bootstrap-cluster-dev
-make run-instrumented || true
+make run-instrumented
 make e2e-test
-make stop-instrumented
+make stop-instrumented || travis_terminate 0
