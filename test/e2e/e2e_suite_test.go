be5b6dbb66440d705370637099fc6e0f9dac9bd5
Will Kutler <willkutler@gmail.com>
2021-04-27 11:36:51 -0600

    templatize secret test
    Signed-off-by: Will Kutler <willkutler@gmail.com>

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -46,6 +46,7 @@ var (
 	gvrRole               schema.GroupVersionResource
 	gvrNS                 schema.GroupVersionResource
 	gvrSCC                schema.GroupVersionResource
+	gvrSecret             schema.GroupVersionResource
 
 	defaultImageRegistry string
 )
@@ -69,6 +70,7 @@ var _ = BeforeSuite(func() {
 	gvrRole = schema.GroupVersionResource{Group: "rbac.authorization.k8s.io", Version: "v1", Resource: "roles"}
 	gvrConfigPolicy = schema.GroupVersionResource{Group: "policy.open-cluster-management.io", Version: "v1", Resource: "configurationpolicies"}
 	gvrSCC = schema.GroupVersionResource{Group: "security.openshift.io", Version: "v1", Resource: "securitycontextconstraints"}
+	gvrSecret = schema.GroupVersionResource{Group: "", Version: "v1", Resource: "secrets"}
 	clientManaged = NewKubeClient("", kubeconfigManaged, "")
 	clientManagedDynamic = NewKubeClientDynamic("", kubeconfigManaged, "")
 	defaultImageRegistry = "quay.io/open-cluster-management"
