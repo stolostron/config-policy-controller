837eac87809ab4925099330247b2e0fd9faf89b8
Gus Parvin <gparvin@redhat.com>
2021-06-23 15:59:44 -0400

    re-organize checks based on review comments
    Signed-off-by: Gus Parvin <gparvin@redhat.com>

--- pkg/controller/configurationpolicy/configurationpolicy_controller.go
+++ pkg/controller/configurationpolicy/configurationpolicy_controller.go
@@ -481,34 +481,31 @@ func createInformStatus(mustNotHave bool, numCompliant int, numNonCompliant int,
 	if kind == "" {
 		return
 	}
-	// We want resources to exist, but none were found
-	if !mustNotHave && numCompliant == 0 && numNonCompliant == 0 {
-		//noncompliant; musthave and objects do not exist
-		update = createMustHaveStatus(desiredName, kind, nonCompliantObjects, namespaced,
-			plc, indx, compliant)
+
+	if mustNotHave {
+		if numNonCompliant > 0 { // We want no resources, but some were found
+			//noncompliant; mustnothave and objects exist
+			update = createMustNotHaveStatus(kind, nonCompliantObjects, namespaced, plc, indx, compliant)
+		} else if numNonCompliant == 0 {
+			//compliant; mustnothave and no objects exist
+			compliant = true
+			update = createMustNotHaveStatus(kind, compliantObjects, namespaced, plc, indx, compliant)
+		}
+	} else { // !mustNotHave (musthave)
+		if numCompliant == 0 && numNonCompliant == 0 { // Special case: No resources found is NonCompliant
+			//noncompliant; musthave and objects do not exist
+			update = createMustHaveStatus(desiredName, kind, nonCompliantObjects, namespaced,
+				plc, indx, compliant)
+		} else if numNonCompliant > 0 {
+			//noncompliant; musthave and some objects do not exist
+			update = createMustHaveStatus("", kind, nonCompliantObjects, namespaced, plc, indx, compliant)
+		} else { // Found only compliant resources (numCompliant > 0 and no NonCompliant)
+			//compliant; musthave and objects exist
+			compliant = true
+			update = createMustHaveStatus("", kind, compliantObjects, namespaced, plc, indx, compliant)
+		}
 	}
-	// We want no resources, but some were found
-	if mustNotHave && numNonCompliant > 0 {
-		//noncompliant; mustnothave and objects exist
-		update = createMustNotHaveStatus(kind, nonCompliantObjects, namespaced, plc, indx, compliant)
-	}
-	// We want resources and we found them
-	if !mustNotHave && numCompliant > 0 && numNonCompliant == 0 {
-		//compliant; musthave and objects exist
-		compliant = true
-		update = createMustHaveStatus("", kind, compliantObjects, namespaced, plc, indx, compliant)
-	}
-	// We want resources and we found some that don't match what we want
-	if !mustNotHave && numNonCompliant > 0 {
-		//noncompliant; musthave and some objects do not exist
-		update = createMustHaveStatus("", kind, nonCompliantObjects, namespaced, plc, indx, compliant)
-	}
-	// We want no resources and none were found
-	if mustNotHave && numNonCompliant == 0 {
-		//compliant; mustnothave and no objects exist
-		compliant = true
-		update = createMustNotHaveStatus(kind, compliantObjects, namespaced, plc, indx, compliant)
-	}
+
 	if update {
 		//update parent policy with violation
 		eventType := eventNormal
