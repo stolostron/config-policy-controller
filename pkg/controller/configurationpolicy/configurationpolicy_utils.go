46cc50554739ca6d027a8e469c32b8c612c6d74f
Will Kutler <wkutler@redhat.com>
2022-02-03 11:43:28 -0500

    add namespaced check before appending ns string to status msg
    Signed-off-by: Will Kutler <wkutler@redhat.com>

--- pkg/controller/configurationpolicy/configurationpolicy_utils.go
+++ pkg/controller/configurationpolicy/configurationpolicy_utils.go
@@ -398,7 +398,7 @@ func createMustHaveStatus(desiredName string, kind string, complianceObjects map
 	// Noncompliant with no resources -- return violation immediately
 	if !compliant && desiredName == "" {
 		message := fmt.Sprintf("No instances of `%v` found as specified", kind)
-		if len(sortedNamespaces) > 0 {
+		if namespaced && len(sortedNamespaces) > 0 {
 			message = fmt.Sprintf("No instances of `%v` found as specified in namespaces: %v",
 				kind, strings.Join(sortedNamespaces, ", "))
 		}
