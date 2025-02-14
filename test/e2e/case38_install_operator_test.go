ea6f662e279ce9fac7bb488339853dfc8ad23f63
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2025-02-14 15:51:16 -0500

    Bump Quay version
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- test/e2e/case38_install_operator_test.go
+++ test/e2e/case38_install_operator_test.go
@@ -3599,7 +3599,7 @@ var _ = Describe("Testing OperatorPolicy", Ordered, Label("supports-hosted"), fu
 		const (
 			opPolYAML     = "../resources/case38_operator_install/operator-policy-no-group-enforce.yaml"
 			opPolName     = "oppol-no-group-enforce"
-			latestQuay310 = "quay-operator.v3.10.7"
+			latestQuay310 = "quay-operator.v3.10.8"
 		)
 
 		BeforeEach(func() {
